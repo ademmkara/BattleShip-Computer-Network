c89c66811662d094dbcf7deaebabae94a8b6e768
Adem Kara <ademkaraa49@gmail.com>
2025-05-13 19:50:21 +0300

    Client connection
    Client bağlantısı oluşturuldu

--- adem_kara_2121221040_networkslab_2025_proje_client/src/main/java/battleshipclient/Client.java
+++ adem_kara_2121221040_networkslab_2025_proje_client/src/main/java/battleshipclient/Client.java
@@ -4,11 +4,45 @@
  */
 package battleshipclient;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author cvsbilisim
  */
+class Listen extends Thread {}
 public class Client {
 
-    
+    //her clientın bir soketi olmalı
+    public static Socket socket;
+
+    //verileri almak için gerekli nesne
+    public static ObjectInputStream sInput;
+    //verileri göndermek için gerekli nesne
+    public static ObjectOutputStream sOutput;
+    //serverı dinleme thredi 
+    public static Listen listenMe;
+
+    public static void Start(String ip, int port) {
+        try {
+            // Client Soket nesnesi
+            Client.socket = new Socket(ip, port);
+          
+            // input stream
+            Client.sInput = new ObjectInputStream(Client.socket.getInputStream());
+            // output stream
+            Client.sOutput = new ObjectOutputStream(Client.socket.getOutputStream());
+            Client.listenMe = new Listen();
+            Client.listenMe.start();
+
+        } catch (IOException ex) {
+            System.out.println("[CLIENT] Error in Start: " + ex.getMessage());
+            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
